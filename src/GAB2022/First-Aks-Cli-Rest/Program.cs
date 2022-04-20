9ae3812085a1c73109d3e7174d19b0172c33ea2e
Bojan Vrhovnik <bojan@vrhovnik.net>
2022-04-20 14:34:18 +0200

    adding REST calls to access master node and execute common commands to get pods back

---
+++ src/GAB2022/First-Aks-Cli-Rest/Program.cs
@@ -0,0 +1,58 @@
+﻿using First_Aks_Cli_Rest;
+using Newtonsoft.Json;
+using Spectre.Console;
+
+var rule = new Rule("[blue]Calling AKS cluster and getting information[/]")
+{
+    Alignment = Justify.Left,
+}.RuleStyle("grey");
+AnsiConsole.Write(rule);
+
+//disable https client cert validation
+var clientHandler = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+};
+
+// set cluster address to connect to the cluster
+var serverUrl = Environment.GetEnvironmentVariable("ClusterBaseAddress") ??
+                throw new ArgumentNullException("Server address was not provided.");
+
+using var client = new HttpClient(clientHandler);
+client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+//provide bearer token for authentication
+var bearerToken = Environment.GetEnvironmentVariable("BearerToken");
+
+var namespaceName = AnsiConsole.Ask("Provide namespace name to traverse through pods", string.Empty);
+
+if (string.IsNullOrEmpty(namespaceName))
+{
+    AnsiConsole.WriteLine("Namespace was not provided, continuing with default namespace");
+    namespaceName = "default";
+}
+
+var requestData = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri($"{serverUrl}api/v1/namespaces/{namespaceName}/pods", UriKind.RelativeOrAbsolute)
+};
+requestData.Headers.TryAddWithoutValidation("Authorization", $"Bearer {bearerToken}");
+
+var result = await client.SendAsync(requestData);
+
+var receivedPods = await result.Content.ReadAsStringAsync();
+
+var pods = JsonConvert.DeserializeObject<Pods>(receivedPods);
+
+var table = new Table();
+table.Border(TableBorder.Ascii2);
+
+table.AddColumn(new TableColumn("Pod name").Centered());
+
+foreach (var pod in pods.Items)
+{
+    table.AddRow(pod.Metadata.Name);
+}
+
+AnsiConsole.Render(table);
---
+++ src/GAB2022/First-Aks-Cli-Rest/init-sample-workloads.ps1
@@ -0,0 +1,11 @@
+﻿# check 0-Demo-Initial/0-aks-basic-setup-script.ps1 for pre-requisites
+
+# create namespace via kubectl
+kubectl create namespace rest
+
+# create few pods to play around with REST
+kubectl run rest-demo-1 --image=<acr-name>.azurecr.io/hello-world:latest --port=80 --namespace=rest
+kubectl run rest-demo-2 --image=<acr-name>.azurecr.io/hello-world:latest --port=80 --namespace=rest
+
+# delete namespace test
+kubectl delete namespace rest
