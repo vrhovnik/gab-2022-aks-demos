c78126fd2cbd9baca4995c7296460b13ced8c5d4
Bojan Vrhovnik <bojan@vrhovnik.net>
2022-05-06 21:59:45 +0200

    added testing namespace as environment variable to be able to modify it without changing the code itself

--- src/GAB2022/Second-Aks-Cli-Managed/Program.cs
+++ src/GAB2022/Second-Aks-Cli-Managed/Program.cs
@@ -22,9 +22,10 @@ foreach (var ns in namespaces.Items)
 
 Console.Read(); //break for continue
 
-HorizontalRule("List pods in namespace - app");
+var namespaceToList = Environment.GetEnvironmentVariable("NamespaceToList") ?? "default";
 
-var listPodInNamespace = await client.ListNamespacedPodAsync("app");
+HorizontalRule($"List pods in namespace - {namespaceToList}");
+var listPodInNamespace = await client.ListNamespacedPodAsync(namespaceToList);
 
 var table = new Table();
 table.Border(TableBorder.Rounded);
@@ -49,7 +50,7 @@ Console.Read(); //break for continue
 
 HorizontalRule("Creating namespace and pod");
 
-var namespaceNameForTest = "test";
+var namespaceNameForTest = Environment.GetEnvironmentVariable("TestNamespace") ?? "test";
 var newNamespace = new V1Namespace { Metadata = new V1ObjectMeta { Name = namespaceNameForTest } };
 
 var resultNamespaceCreated = await client.CreateNamespaceAsync(newNamespace);
@@ -93,11 +94,11 @@ demux.Start();
 
 var buff = new byte[4096];
 var stream = demux.GetStream(1, 1);
-await stream.ReadAsync(buff, 0, 4096)
+await stream.ReadAsync(buff.AsMemory(0, 4096))
     .ConfigureAwait(false);
 
 var str = System.Text.Encoding.Default.GetString(buff);
-Console.WriteLine(str); //ouput ls command
+AnsiConsole.WriteLine(str); //ouput ls command
 
 Console.Read(); //break for continue
 
@@ -121,7 +122,7 @@ var typeMap = new Dictionary<string, Type>
 var yamlPath = Path.Join(Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location), "sample.yaml");
 var objects = await KubernetesYaml.LoadAllFromFileAsync(yamlPath, typeMap);
 
-foreach (var obj in objects) Console.WriteLine(obj);
+foreach (var obj in objects) AnsiConsole.WriteLine(obj.ToString() ?? "No object defined");
 
 Console.Read(); //stop and press key to continue
 
@@ -130,13 +131,13 @@ HorizontalRule("Watching pods - watch pods");
 var podlistResp = client.ListNamespacedPodWithHttpMessagesAsync("default", watch: true);
 using (podlistResp.Watch<V1Pod, V1PodList>((type, item) =>
        {
-           Console.WriteLine("==on watch event==");
+           AnsiConsole.WriteLine("==on watch event==");
            Console.WriteLine(type);
-           Console.WriteLine(item.Metadata.Name);
-           Console.WriteLine("==on watch event==");
+           AnsiConsole.WriteLine(item.Metadata.Name);
+           AnsiConsole.WriteLine("==on watch event==");
        }))
 {
-    Console.WriteLine("press ctrl + c to stop watching");
+    Console.WriteLine("press CTRL+C to stop watching");
 
     var ctrlc = new ManualResetEventSlim(false);
     Console.CancelKeyPress += (_, _) => ctrlc.Set();
